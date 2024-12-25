4a96df9a64113539d7f550e829317791f353d845
Naveen pandian <npandian515@gmail.com>
2024-12-25 23:01:13 +0530

    new repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <div className='grid grid-cols-12'>
+        <div className='md:bg-green-300 sm:bg-red-300 bg-blue-300 col-span-10 sm: col-span-5'>
+          ki
+        </div>
+        <div className='bg-yellow-300 col-span-2'>
+          kii
+        </div>    
+      </div>
+    </div>
+  );
+}
+
+export default App;
