a8970c7765d500543f0a3c4258f7b6634fbe5d64
Naveen pandian <npandian515@gmail.com>
2024-12-25 23:04:04 +0530

    1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
         <div className='md:bg-green-300 sm:bg-red-300 bg-blue-300 col-span-10 sm: col-span-5'>
           ki
         </div>
-        <div className='bg-yellow-300 col-span-2'>
+        <div className='bg-yellow-300 col-span-2 text-2xl rounded-full'>
           kii
         </div>    
       </div>
